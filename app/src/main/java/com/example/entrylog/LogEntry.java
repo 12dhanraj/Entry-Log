79bdf23d5305ee22ebff5d803892592a5d37a503
12dhanraj <dhanrajlatish12@gmail.com>
2024-06-29 14:09:36 +0530

    JSON connected

--- app/src/main/java/com/example/entrylog/LogEntry.java
+++ app/src/main/java/com/example/entrylog/LogEntry.java
@@ -15,9 +15,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LogEntry extends AppCompatActivity {
     EditText e1,e2,e3,e4;
     AppCompatButton b1,b2;
+    String apiUrl="http://10.0.4.16:3000/api/students";
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +44,46 @@ public class LogEntry extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //value reading
                 String getName=e1.getText().toString();
                 String getAdmissionNo=e2.getText().toString();
                 String getSystemNo=e3.getText().toString();
                 String getDepartment=e4.getText().toString();
-                Toast.makeText(getApplicationContext(),getName+" "+getAdmissionNo+" "+getSystemNo+" "+getDepartment,Toast.LENGTH_LONG).show();
+
+                //json object creation
+                JSONObject student=new JSONObject();
+                try {
+                    student.put("name",getName);
+                    student.put("admission_number",getAdmissionNo);
+                    student.put("system_number",getSystemNo);
+                    student.put("department",getDepartment);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                //JSON object request creation
+                JsonObjectRequest jasonObjectRequest=new JsonObjectRequest(
+
+                        Request.Method.POST,
+                        apiUrl,
+                        student,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(getApplicationContext(), "Added Successfully", Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                );
+                //request queue
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(jasonObjectRequest);
+
             }
         });
         b2.setOnClickListener(new View.OnClickListener() {
